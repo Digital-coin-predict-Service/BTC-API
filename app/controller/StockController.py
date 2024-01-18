f716a464619bcd9c4a1fba059df5f6c1d9665237
kwonsw812 <kwonsw812@gmai.com>
2024-01-18 12:55:11 +0900

    new BTC model

--- app/controller/StockController.py
+++ app/controller/StockController.py
@@ -2,22 +2,34 @@ import json
 
 from flask import Flask, jsonify
 from app.service.StockService import get_hello_message
-from app.Prediction.main import prediction
+from app.Prediction.main import *
 from app.config import db, app
 from app.Entity.Stock import Stock
 
+
 # app = Flask(__name__)
 
-@app.route('/prediction/BTC')
-def stockPrediction():
-    BTC_prediction = prediction()
+@app.route('/prediction')
+def Prediction():
+    prediction = btc_prediction()
 
-    result = Stock.query.filter_by(name="BTC").first()
-    path = result.path
+    # result = Stock.query.filter_by(name="BTC").first()
+    # path = result.path
+    #
+    # with open(path, 'w') as f:
+    #     json.dump(prediction, f, indent=2)
+    #
+    # response = app.response_class(status=200)
+    #
+    # return response
 
-    with open(path, 'w') as f:
-        json.dump(BTC_prediction, f, indent=2)
 
-    response = app.response_class(status=200)
-
-    return response
+# @app.route('/<name>')
+# def findStockPathByName(name):
+#     result = Stock.query.filter_by(name=name).first()
+#     path = result.path
+#
+#     with open(path, 'r') as json_file:
+#         data = json.load(json_file)
+#
+#     return jsonify(data)
